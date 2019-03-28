4633e53e671ddc90b2a32152064be2df7cca62a2
jcwong2 <jcwong2@ualberta.ca>
2019-03-28 17:56:12 -0600

    Fixed indent.

--- WindyWorld/windy_navigation.py
+++ WindyWorld/windy_navigation.py
@@ -203,7 +203,7 @@ def main():
         mean_rwd_freq_mean[j], stde_rwd_freq_mean[j] = rl_experiment(FrequencyMeanAgent, N, alpha_val, GAMMA, 1, FREQ_SIGMA_FACTOR, EPSILON, WindyEnvironment, NUM_EPISODES, NUM_RUNS)
 
     # Plot final results
-        plt.figure(figsize=(18.5, 10.5))
+    plt.figure(figsize=(18.5, 10.5))
     plt.plot(ALPHA, mean_rwd_ep, label=r'Dynamic $\sigma$ (Episode)')
     plt.plot(ALPHA, mean_rwd_freq_raw, label=r'Dynamic $\sigma$ (Frequency, Raw)')
     plt.plot(ALPHA, mean_rwd_freq_mean, label=r'Dynamic $\sigma$ (Frequency, Mean)')
